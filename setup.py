5c1adda964ead8815dc5073beaa1b3f2b8bd634d
Hudson <hudson.lekunze@outlook.com>
2021-02-19 13:52:47 +0200

    added setup config with minimal descriptions

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+from setuptools import setup
+
+setup(
+	name='cvi',
+	version='0.0.1',
+	description='Internal Validity Indices for Cluster Evaluation',
+	author='Hudson Taylor Lekunze',
+	author_email='hudson.lekunze@outlook.com',
+	packages=['cvi'],
+)
+
